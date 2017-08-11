c2189d5f50c49a744b8fac5c0defb5b50c77dad2
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-08-11 09:40:50 -0700

    script with hashed value was added

--- atomic-swaps-script.js
+++ atomic-swaps-script.js
@@ -16,22 +16,23 @@ var masterKey = bitcoin.HDNode.fromSeedBuffer(Buffer.from('zzzzzzzzzzzzzzzzzzzzz
 // var aliceAddress = alicePrivKey.getAddress();
 // var bobAddress = bobPrivKey.getAddress();
 
+let aliceSecret = Buffer.from('zzzzzzzzzzzzzzzzzzzzzzzzzz');
+let aliceSecretHash = bitcoin.crypto.hash160(aliceSecret);
+console.log('alice secret = ', aliceSecret);
 
-function generateKey() {
-  return require('randombytes')(32);
-}
+let addressToSentromScript = 'myUMzCSWHivWdv4MFNygdqeTh17N3G74k3'
 
-function createP2SH(aliceSecret) {
-  //var aliceSecret = require('randombytes')(32);
-  console.log('alice secret = ', aliceSecret);
-  var aliceSecretHash = bitcoin.crypto.hash160(aliceSecret);
+var aliceToBobRedeemScript = bitcoin.script.compile([
+    bitcoin.opcodes.OP_HASH160,
+    //bitcoin.script.number.encode(1221212),
+    aliceSecretHash,
+    bitcoin.opcodes.OP_EQUAL
+]);
 
-  console.log('alice secret = ', aliceSecretHash);
-  var aliceToBobRedeemScript = bitcoin.script.compile([
-      bitcoin.opcodes.OP_HASH160,
-      aliceSecretHash,
-      bitcoin.opcodes.OP_EQUAL
-  ]);
+
+function createP2SH() {
+
+  //console.log('SCRIPT = ', bitcoin.script.toASM(aliceToBobRedeemScript));
 
   var aliceToBobScriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(aliceToBobRedeemScript))
   var aliceToBobP2SHAddress = bitcoin.address.fromOutputScript(aliceToBobScriptPubKey, testnet)
@@ -39,11 +40,11 @@ function createP2SH(aliceSecret) {
   console.log('aliceToBobP2SHAddress; ', aliceToBobP2SHAddress);
 }
 
-function redeemBTCFromScript(txid, addressToSentromScript, aliceSecret) {
+function redeemBTCFromScript(txid, inputNum) {
   var redeemScript = aliceToBobRedeemScript
 
   var txb = new bitcoin.TransactionBuilder(testnet)
-  txb.addInput(txid, 1)
+  txb.addInput(txid, inputNum)
   txb.addOutput(addressToSentromScript, 700)
 
   var txRaw = txb.buildIncomplete()
@@ -53,9 +54,15 @@ function redeemBTCFromScript(txid, addressToSentromScript, aliceSecret) {
   ], redeemScript)
   txRaw.setInputScript(0, redeemScriptSig)
 
+  //console.log('SCRIPT = ', bitcoin.script.toASM(redeemScriptSig));
+
 
   console.log("raw transaction hex = " + txRaw.toHex());
 }
+
+createP2SH();
+redeemBTCFromScript('8e13d78ec4112abf7f6cfb6a5ecf7ba759a278884316c7b9d9134b1abfc8830f', 0)
+
 
 
 
